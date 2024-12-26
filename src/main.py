c8f7110cdb7c43d342144992401708b13327f223
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2024-12-26 17:56:52 +0800

    created a table in data.db

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 import tkinter
 import sqlite3
 
-def main():
+
+def init():
     connection = sqlite3.connect('data/data.db')
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE finance_tracker(
+            type text,
+            ammount text,
+            category text,
+            date text
+        ) 
+        """
+    )
+    connection.commit()
+    return connection, cursor
+
+
+def main():
+    connection, cursor = init()
     root = tkinter.Tk()
     root.title('PESOurce')
     root.geometry('1000x700')
