25398434388a6abf28c4708dd604d8abf18e89cb
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2024-12-26 11:08:13 +0800

    added a main window with tkinter

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import tkinter
 
 def main():
-    pass
+    root = tkinter.Tk()
+    root.title('PESOurce')
+    root.geometry('1000x700')
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
